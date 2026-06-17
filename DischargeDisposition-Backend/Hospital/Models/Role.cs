cd9cd370be69c052a39fa18a3f5e8f2990ca32be
Korem Hemanth Sai Reddy <hemanthsaireddy09@gmail.com>
2026-06-17 03:06:48 -0700

    Added Models- User, Department, Role, Plan(Insurance)

---
+++ DischargeDisposition-Backend/Hospital/Models/Role.cs
@@ -0,0 +1,17 @@
+﻿//Role.cs
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace DischargeDisposition_Backend.Hospital.Models
+{
+    public class Role
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public byte RoleId { get; set; } // SQL: TINYINT IDENTITY(1,1)
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+        public ICollection<User> Users { get; set; } = new List<User>();
+
+    }
+}
