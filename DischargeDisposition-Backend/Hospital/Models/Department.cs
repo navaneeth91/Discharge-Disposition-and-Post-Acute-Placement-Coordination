764603fb542ad0a73b5899883727a915b55fd10e
Korem Hemanth Sai Reddy <hemanthsaireddy09@gmail.com>
2026-06-17 03:06:48 -0700

    Added Models- User, Department, Role, Plan(Insurance)

---
+++ DischargeDisposition-Backend/Hospital/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DischargeDisposition_Backend.Hospital.Models
+{
+    public class Department
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public byte DeptId { get; set; } // TINYINT IDENTITY(1,1)
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+        public ICollection<User> Users { get; set; } = new List<User>();
+        public ICollection<Patient> Patients { get; set; } = new List<Patient>();
+    }   
+}
---
+++ DischargeDisposition-Backend/Hospital/Models/User.cs
@@ -0,0 +1,60 @@
+﻿
+//User.cs
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace DischargeDisposition_Backend.Hospital.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(15)]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [MaxLength(255)]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        public byte DeptId { get; set; }
+
+        [ForeignKey(nameof(DeptId))]
+        public Department department { get; set; } = null!;
+
+        [Required]
+        public byte RoleId { get; set; }
+
+        [ForeignKey(nameof(RoleId))]
+        public Role role { get; set; } = null!;
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        [MaxLength(60)]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public ICollection<PatientDelay> PatientDelays { get; set; } = new List<PatientDelay>();
+
+        public ICollection<DispositionDecision> DispositionDecisions { get; set; } = new List<DispositionDecision>();
+
+        public ICollection<Referral> Referrals { get; set; } = new List<Referral>();
+    }
+}
