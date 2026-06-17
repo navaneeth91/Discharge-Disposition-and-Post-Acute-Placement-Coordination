52ba81968e511f30c019dc8561a78d9b67de5971
Navaneeth Siliveri <navaneethsiliveri7288@gmail.com>
2026-06-17 15:38:10 +0530

    Added Models:CoverageRule,AuthorizationDecision,referral,Patientdelay

---
+++ DischargeDisposition-Backend/Hospital/Models/Referral.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DischargeDisposition_Backend.Models;
+
+public class Referral
+{
+    [Key]
+    public int ReferralId { get; set; }
+
+    [Required]
+    public int PatientId { get; set; }
+
+    [Required]
+    public int ProviderId { get; set; }
+
+    [Required]
+    public int CareManagerId { get; set; }
+
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+    [Required]
+    [MaxLength(20)]
+    public AuthorizationStatus Status { get; set; } 
+
+    [Required]
+    [MaxLength(10)]
+    public Priority Priority { get; set; } 
+
+    // Navigation Properties
+
+    [ForeignKey(nameof(PatientId))]
+    public Patient patient { get; set; } = null!;
+
+    [ForeignKey(nameof(ProviderId))]
+    public PostAcuteProvider provider { get; set; } = null!;
+
+    [ForeignKey(nameof(CareManagerId))]
+    public User careManager { get; set; } = null!;
+    public virtual AuthorizationTracking? AuthorizationTrackings {get;set; }
+}
---
+++ DischargeDisposition-Backend/Insurance/Models/CoverageRule.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DischargeDisposition_Backend.Insurance.Models;
+
+public class CoverageRule
+{
+    [Key]
+    public int RuleId { get; set; }
+
+    [Required]
+    public int PlanId { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string ServiceType { get; set; } = string.Empty;
+
+    [Required]
+    public bool RequiresAuthorization { get; set; }
+
+    // Navigation Property
+    [ForeignKey(nameof(PlanId))]
+    public Plan plan { get; set; } = null!;
+}
