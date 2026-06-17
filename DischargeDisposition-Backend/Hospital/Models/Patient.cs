949f704004df727e7492de22ec654fcc8b398268
Yenkamolla Ankitha <yankithareddy@gmail.com>
2026-06-17 15:39:18 +0530

    Added Models : Patient, LengthOfStayTracking

---
+++ DischargeDisposition-Backend/Hospital/Models/Patient.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DischargeDisposition_Backend.Hospital.Models
+{
+    public class Patient
+    {
+        [Key]
+        public long PatientId { get; set; }   // BIGINT PK
+
+        [Required]
+        [MaxLength(20)]
+        public string Mrn { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        public DateOnly Dob { get; set; }
+
+        [Required]
+        public DateTime AdmissionDate { get; set; }
+
+        [Required]
+        public DateOnly ExpectedDischargeDate { get; set; }
+
+        [Required]
+        public DateTime? ActualDischargeDate { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }   // M, F, O
+
+        [MaxLength(255)]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [MaxLength(15)]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        public byte IsActive { get; set; } = 1;
+
+        [Required]
+        public byte DeptId { get; set; }
+
+        public Department department { get; set; } = null!;
+
+        public ICollection<DispositionDecision> DispositionDecisions { get; set; } = new List<DispositionDecision>();
+
+        public ICollection<Referral> Referrals { get; set; } = new List<Referral>();
+
+        public ICollection<AuthorizationTracking> AuthorizationTrackings { get; set; } = new List<AuthorizationTracking>();
+
+        public ICollection<PatientDelay> PatientDelays { get; set; }  = new List<PatientDelay>();
+
+
+
+        public LengthOfStayTracking? LengthOfStayTracking { get; set; }
+
+    }
+}
