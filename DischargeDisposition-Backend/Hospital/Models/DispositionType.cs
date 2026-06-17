3a199886ba60bccff678de0093c7ba4da54f6e8a
supraja425 <suprajayelagandula@gmail.com>
2026-06-17 03:11:04 -0700

    Added models DispositionDecision DispositionType PostAcuteProvider InsuranceProvider

---
+++ DischargeDisposition-Backend/Hospital/Models/DispositionType.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DischargeDisposition_Backend.Models
+{
+    public class DispositionType
+    {
+        [Key]
+        public int DispositionTypeId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public String DispositionName { get; set; }
+
+        public virtual ICollection<PostAcuteProvider> PostAcuteProviders { get; set; }
+        = new List<PostAcuteProvider>();
+
+        
+        public virtual ICollection<DispositionDecision> DispositionDecisions { get; set; }
+            = new List<DispositionDecision>();
+
+        public virtual ICollection<AuthorizationTracking> AuthorizationTrackings { get; set; }
+        = new List<AuthorizationTracking>();
+    }
+}
