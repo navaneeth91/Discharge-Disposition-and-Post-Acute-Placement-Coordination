7f9ac1c7bc6e3389666a750f2a1c0c3f2a28ba81
Praneeth <praneethmangipudi@gmail.com>
2026-06-27 20:35:03 +0530

    Fixed Sidebar and dashboardService issues

--- Frontend/src/components/dashboard/PatientChart.vue
+++ Frontend/src/components/dashboard/PatientChart.vue
@@ -93,8 +93,6 @@ async function loadChart() {
             await getPatientDistribution()
         const patients = response.data.data
 
-        const patients = response.data.data
-
         chartData.value.labels =
             patients.map(
                 x => x.departmentName
